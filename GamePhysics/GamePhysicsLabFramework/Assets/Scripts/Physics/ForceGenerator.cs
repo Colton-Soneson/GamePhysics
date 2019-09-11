99fb371297068c98633f703025cf2b5d01bd5664
Colton Soneson <colton.soneson@mymail.champlain.edu>
2019-09-10 23:14:36 -0400

    Lab One

---
+++ GamePhysics/GamePhysicsLabFramework/Assets/Scripts/Physics/ForceGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceGenerator
+{
+    public static Vector2 GenerateForce_Gravity(float particleMass, float gravitationalConstant, Vector2 WorldUp)
+    {
+        // f_gravity: f = mg
+        Vector2 f_gravity = particleMass * gravitationalConstant * WorldUp;
+        return f_gravity;
+    }
+}
---
+++ GamePhysics/weekOne/Assets/Scripts/PaddleControl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleControl : MonoBehaviour
+{
+    public GameObject PaddleOne;
+    public GameObject PaddleTwo;
+
+    Quaternion paddleOneStartMark;
+    Quaternion paddleTwoStartMark;
+
+    Quaternion paddleOneEndMark;
+    Quaternion paddleTwoEndMark;
+
+    public float speed = 1.0F;
+    private float startTime;
+
+    private float journeyLengthP1;
+    private float journeyLengthP2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //startTime = Time.time;
+
+        startTime = 0.0f;
+
+        paddleOneStartMark = PaddleOne.GetComponent<Transform>().rotation;
+        paddleTwoStartMark = PaddleTwo.GetComponent<Transform>().rotation;
+
+ 
+
+        paddleOneEndMark = new Quaternion(37.334f, -29.231f, -18.746f, PaddleOne.GetComponent<Transform>().rotation.w);
+        paddleTwoEndMark = new Quaternion(37.453f, 28.786f, 18.476f, PaddleTwo.GetComponent<Transform>().rotation.w);
+
+        //journeyLengthP1 = Quaternion.Slerp(paddleOneStartMark, paddleOneEndMark);
+        //journeyLengthP2 = Quaternion.Distance(paddleTwoStartMark, paddleTwoEndMark);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            PaddleOne.GetComponent<Transform>().rotation = Quaternion.Slerp(paddleOneStartMark, paddleOneEndMark, startTime);
+        }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            PaddleTwo.GetComponent<Transform>().rotation = Quaternion.Slerp(paddleTwoStartMark, paddleTwoEndMark, startTime);
+        }
+
+        startTime = startTime + Time.deltaTime;
+    }
+}
